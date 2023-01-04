6c1efeb79238c9dcfbbf7ba64f038a168a1cb3a1
Siddesh <siddeshsankhya@gmail.com>
2023-01-04 22:01:02 +0530

    added huddle01 video calling for appointments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Room from "./components/Room";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Room />}></Route>
+        <Route path="/:wallet" element={<Room />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/components/Room.jsx
@@ -0,0 +1,93 @@
+import "../App.css";
+
+import {
+  HuddleClientProvider,
+  getHuddleClient,
+} from "@huddle01/huddle01-client";
+
+import { useHuddleStore } from "@huddle01/huddle01-client/store";
+import PeerVideoAudioElem from "./PeerVideoAudioElem";
+import MeVideoElem from "./MeVideoElem";
+
+function Room() {
+  const huddleClient = getHuddleClient(
+    "07c420f6af49358778b3b1a040f711bf88144d15de50766409941663c64689c9"
+  );
+  const peersKeys = useHuddleStore((state) => Object.keys(state.peers));
+  const lobbyPeers = useHuddleStore((state) => state.lobbyPeers);
+  const roomState = useHuddleStore((state) => state.roomState);
+  const recordingState = useHuddleStore((state) => state.recordingState);
+  const recordings = useHuddleStore((state) => state.recordings);
+
+  const handleJoin = async () => {
+    try {
+      await huddleClient.join("dev", {
+        address: "0x15900c698ee356E6976e5645394F027F0704c8Eb",
+        wallet: "",
+        ens: "axit.eth",
+      });
+
+      console.log("joined");
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  return (
+    <HuddleClientProvider value={huddleClient}>
+      <div>
+        <div className="card">
+          <button onClick={handleJoin}>Join Room</button>
+          <button onClick={() => huddleClient.enableWebcam()}>
+            Enable Webcam
+          </button>
+          <button onClick={() => huddleClient.disableWebcam()}>
+            Disable Webcam
+          </button>
+          <button onClick={() => huddleClient.allowAllLobbyPeersToJoinRoom()}>
+            allowAllLobbyPeersToJoinRoom()
+          </button>
+          <button
+            onClick={() =>
+              // will not work in localhost
+              huddleClient.startRecording({
+                sourceUrl: window.location.href,
+              })
+            }
+          >
+            startRecording()
+          </button>
+          <button onClick={() => huddleClient.stopRecording({ ipfs: true })}>
+            stopRecording()
+          </button>
+        </div>
+
+        <MeVideoElem />
+
+        {lobbyPeers[0] && <h2>Lobby Peers</h2>}
+        <div>
+          {lobbyPeers.map((peer) => (
+            <div>{peer.peerId}</div>
+          ))}
+        </div>
+
+        {peersKeys[0] && <h2>Peers</h2>}
+
+        <div className="peers-grid">
+          {peersKeys.map((key) => (
+            <PeerVideoAudioElem key={`peerId-${key}`} peerIdAtIndex={key} />
+          ))}
+        </div>
+        <div className="text-blue">
+          <h2>Recording State</h2>
+          <h3>inProgress: {recordingState.inProgress.toString()}</h3>
+          <h3>processing: {recordingState.processing.toString()}</h3>
+          <h3>started: {recordingState.started.toString()}</h3>
+          <h3>recordings: {JSON.stringify(recordings)}</h3>
+        </div>
+      </div>
+    </HuddleClientProvider>
+  );
+}
+
+export default Room;
